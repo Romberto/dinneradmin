00c835e351a92013e168d6a551d8e42bf1e2aeb1
Romberto <korhoff@mail.ru>
2025-03-29 00:51:16 +0400

    getAllProduct with category

--- src/components/page/Menu/Menu.tsx
+++ src/components/page/Menu/Menu.tsx
@@ -1,34 +1,46 @@
-import React, { useEffect } from 'react';
-import { CategoryT, FetchResultT, ProductT } from '../../../utils/types';
-import { BASE_URL } from '../../../utils/const';
+import React from "react";
+import { CategoryT, FetchResultT, ProductT } from "../../../utils/types";
+import { useGetCategoryQuery } from "../../../servises/category";
+import { useGetProductsQuery } from "../../../servises/product";
 
 export const Menu: React.FC = () => {
-  useEffect(() => {
-    const fetchMenu = async () => {
-      try {
-        const resCategory = await fetch(BASE_URL.concat("category"));
-        const resProduct = await fetch(BASE_URL.concat("product"));
-        const categoryData = await resCategory.json()
-        const productData = await resProduct.json()
-        const res:FetchResultT = {}
-        categoryData.forEach((cat:CategoryT) => {
-          res[cat.name] = []
-          productData.forEach((product:ProductT) => {
-            if(cat.id === product.category){
-              res[cat.name].push(product)
-            }
-          });
-        });
-        return res
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    };
-    console.log(fetchMenu());
-  }, []);
+  const { data: dataCategory, isLoading: isLoadingCategory } =
+    useGetCategoryQuery(undefined);
+  const { data: dataProduct, isLoading: isLoadingProduct } =
+    useGetProductsQuery(undefined);
+
+  if (isLoadingCategory || isLoadingProduct) return <p>Loading...</p>;
+
+  const createMenuList = (dataC: CategoryT[], dataP: ProductT[]) => {
+    const res: FetchResultT = {};
+    dataC.forEach((cat: CategoryT) => {
+      res[cat.name] = [];
+      dataP.forEach((product: ProductT) => {
+        if (cat.id === product.category) {
+          res[cat.name].push(product);
+        }
+      });
+    });
+    return res;
+  };
+
+  if (!dataCategory || !dataProduct) return <p>No data available</p>;
+
+  const data = createMenuList(dataCategory, dataProduct);
+
   return (
-    <div className='container'>
-        {}
+    <div className="container">
+      {Object.entries(data).map(([categoryName, products]) => (
+        <div key={categoryName}>
+          <h2 className="bg-orange-600 text-white font-bold text-xl py-1.5">{categoryName}</h2>
+          <ul>
+            {products.map((product: ProductT) => (
+              <li key={product.id}>{product.name}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
+
--- src/servises/category.ts
+++ src/servises/category.ts
@@ -2,14 +2,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 import { BASE_URL } from '../utils/const'
-import { ItemT } from '../utils/types'
+import { CategoryT, ItemT } from '../utils/types'
 
 // Define a service using a base URL and expected endpoints
 export const categoryApi = createApi({
   reducerPath: 'category',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
-    getCategories: builder.query<ItemT[], undefined>({
+    getCategory: builder.query<CategoryT[], undefined>({
       query: () => `category/`,
     }),
   }),
@@ -17,4 +17,4 @@ export const categoryApi = createApi({
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetCategoriesQuery } = categoryApi
+export const { useGetCategoryQuery } = categoryApi
